30157a8c361ec5070935cfeff272acd592ee8af3
CharaD7 <jijakahn6@gmail.com>
2024-08-09 10:42:18 +0000

    Updated Naavbar content
    Styled Get Started/Dashboard button with aceternity style

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = async(props: Props) => {
       backdrop-blur-lg z-[100] flex items-center border-b-[1px]
       border-neutral-900 justify-between">
       <aside className="flex items-center gap-[2px]">
-        <p className="text-3xl font-bold">Mini</p>
+        <p className="text-3xl font-bold">Sha</p>
         <Image
           src="/fuzzieLogo.png"
           width={15}
@@ -18,7 +18,7 @@ const Navbar = async(props: Props) => {
           alt="fuzzie logo"
           className="shadow-sm"
         />
-        <p className="text-3xl font-bold">Sass</p>
+        <p className="text-3xl font-bold">am</p>
       </aside>
       <nav className="absolute left-[50%] top-[50%] transform
         translate-x-[50%] translate-y-[-50%] hidden md:block">
@@ -43,6 +43,23 @@ const Navbar = async(props: Props) => {
           </li>
         </ul>
       </nav>
+      <aside className="flex items-center gap-4">
+        <Link
+          href="/dashboard"
+          className="relative inline-flex h-10 overflow-hidden rounded-full
+          p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400
+          focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite]
+            bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center
+            justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium
+            text-white backdrop-blur-3xl">
+            {/* TODO: Wire Up User Handler */}
+            {true ? 'Dashboard' : 'Get Started'}
+          </span>
+        </Link>
+      </aside>
     </header>
   )
 };
