4028cd6027fe7c7b2635c5d1459e89c53c80f7aa
CharaD7 <jijakahn6@gmail.com>
2024-08-09 21:21:01 +0000

    Added shadcn ui button component
    Styled shadcn ui button component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { ContainerScroll } from "@/components/global/container-scroll-animation";
 import Navbar from "@/components/global/navbar";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,7 +11,27 @@ export default function Home() {
         relative flex flex-col items-center antialiased">
         <div className="absolute inset-0 h-full w-full items-center px-5 py-24
           [background:radial-gradient(125%_125%_at_50%,#000_35%,#233_100%)]"></div>
-        <div className="flex flex-col mt-[-100px] md:mt-[-50px]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll
+            titleComponent={
+              <div className="flex items-center justify-center flex-col">
+                <Button
+                  size={'lg'}
+                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full
+                  border-[#4d4d4d] bg-[#1f1f1f] hover:bg-white group transition-all flex
+                  items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500
+                  duration-500"
+                >
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r 
+                  from-neutral-500 to-neutral-600 md:text-center font-sans 
+                  group-hover:bg-gradient-to-r group-hover:from-black group-hover:to-black">
+                    Start For Free Today
+                  </span>
+                </Button>
+              </div>
+            }
+          ></ContainerScroll>
+        </div>
       </section>
     </main>
   );
